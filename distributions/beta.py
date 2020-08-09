d2aad0fac91249fb859e121f5f28857c33b534e0
vwang0 <bwang@go.wustl.edu>
2020-08-09 18:41:04 -0500

    20200809

---
+++ distributions/beta.py
@@ -0,0 +1,36 @@
+"""
+beta distribution
+https://en.wikipedia.org/wiki/Beta_distribution
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+
+def gamma_function(n):
+    cal = 1
+    for i in range(2, n):
+        cal *= i
+    return cal
+
+def beta(x, a, b):
+    '''
+    x:
+    a:
+    b:
+    return: 
+    '''
+    gamma = gamma_function(a + b) / (gamma_function(a) * gamma_function(b))
+    y = gamma * (x**(a - 1)) * ((1 - x)**(b - 1))
+    return x, y, np.mean(y), np.std(y)
+
+
+for ls in [(1, 3), (5, 1), (2, 2), (2, 5)]:
+    a, b = ls[0], ls[1]
+    x = np.arange(0, 1, 0.001, dtype=np.float)
+    x, y, u, s = beta(x, a=a, b=b)
+    plt.plot(x,
+             y,
+             label=r'$\mu=%.2f,\sigma=%.2f,'
+             r'\alpha=%d,\beta=%d$' % (u, s, a, b))
+plt.legend()
+plt.savefig('beta.png')
+plt.show()
